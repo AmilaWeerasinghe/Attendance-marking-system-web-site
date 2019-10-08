96f10621358eb8a321d3b1f24eafb4a094f175e3
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-10-08 20:09:19 +0530

    admin permission class size

--- plistDUP.php
+++ plistDUP.php
@@ -11,7 +11,7 @@ error_reporting(1);
 <tr bgcolor=blue><td align=center><font SIZE=6 color=white>Smart Attendance Marking System</font></td></tr>
 <tr><td><table align=center width=750 cellspacing=0 cellpadding=5>
 <tr><td align=center><a href=dlist.php>Doctors</td><td align=center><a
-href=plist.php>Class details</td><td align=center><a
+href=plist.php>Patients</td><td align=center><a
 href=alist.php>Appointments</td>
 </table></td></tr>
 <tr bgcolor=red><td ><font size=4 color=white>Patient
@@ -41,7 +41,8 @@ Not Found</font></td></tr>";
 <tr bgcolor=red><td><font size=4 color=white>Deleted
 Records</font></td></tr>
 <tr><td><table width=750 cellspacing=0 cellpadding=5>
-<tr bgcolor=#ccccc><td align=center>S No</td><td align=center>Course Name</td><td align=center>Address</td><td
+<tr bgcolor=#ccccc><td align=center>S No</td><td align=center>patient
+Name</td><td align=center>Address</td><td
 align=center>Options</td></tr>
 <?php
 $rs2=mysql_query("SELECT * from patient where pshow='N' order by
