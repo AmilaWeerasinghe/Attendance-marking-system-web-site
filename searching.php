305f4aa5ffb2a9f4efbdc7706a597faaf1317890
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-10-08 15:45:56 +0530

    best

--- searching.php
+++ searching.php
@@ -1,10 +1,29 @@
+
+<form action="searching.php" method="post">
+<select name="class">
+<option value="A">A</option>
+<option value="B">B</option>
+<option value="C">C</option>
+<option value="D">D</option>
+</select>
+<input type="submit" name="submit" value="Get Selected Values" />
+</form>
 <?php
+$selected_val='';
+if(isset($_POST['submit'])){
+$selected_val = $_POST['class'];  // Storing Selected Value In Variable
+echo "You have selected :" .$selected_val;  // Displaying Selected Value
+}
+
 $con=mysqli_connect("localhost","root","");
 mysqli_select_db($con,"attendance");
 $output='';
 $count=0;
 if(isset($_POST['search'])){
     $searchq=$_POST['search'];
+
+    
+    print($selected_val);
     
     $query=mysqli_query($con,"SELECT * FROM mark_attendance where STUDENT_ID like '%$searchq%'");
     $count=mysqli_num_rows($query);
@@ -30,15 +49,30 @@ if(isset($_POST['search'])){
     <title>Searching</title>
 </head>
 <body>
+<br>
+     Enter Your Student ID number
     <form action="searching.php" method="post">
     <input type="text" name="search"/>
     <input type="submit" value=">>"/>
     </form>
-
+    Number of your attended days =
     <?php
-    print("$output  "); 
-    print($_POST['search']);
+
+    //print("$output  "); 
+    //printf($_POST['search'], PHP_EOL);
     print("   $count");
     ?>
+     
+    <br> Attendance Percentage upto now
+    <?php
+$obtained_num=$count;
+$total_num=10;
+$per = ($obtained_num/$total_num)*100;
+
+echo $per;
+
+?>
+
 </body>
 </html>
+
