d1335d0f00bc74e35508ad98667681b13ce0bb88
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-10-13 23:44:20 +0530

    teacher tables implementation

--- ALIST.php
+++ ALIST.php
@@ -1,72 +1,54 @@
 <?php
-require('connection.php');
+require('connection2.php');
 error_reporting(1);
-?>
-<html>
+?><html>
  <head>
-  <title>SAMS</title>
-  <link rel="stylesheet" href="css/bootstrap.css"/>
+  <title>Course Lists</title>
+ <link rel="stylesheet" href="css/bootstrap.css"/>
  </head> 
  <body>
 <table class="table table-bordered">
-
 <tr bgcolor=blue><td align=center><font SIZE=6 color=white>Smart Attendance Marking System</font></td></tr>
 <tr><td><table align=center width=750 cellspacing=0 cellpadding=5>
-<tr><td align=center><a href=dlist.php>Doctors</td><td align=center><a
-href=plist.php>Patients</td><td align=center><a
-href=alist.php>Appointments</td>
+<tr><td align=center><a href=dlist.php>Users</td><td align=center><a
+href=plist.php>Class details</td><td align=center><a
+href=alist.php>Teacher details</td>
 </table></td></tr>
-<tr bgcolor=red><td ><font size=4 color=white>Appointments
+<tr bgcolor=red><td ><font size=4 color=white>Course
 List</font></td></tr>
-<tr><td><a href=app.php>Add New Appointments</a></td></tr>
+<tr><td><a href=padd.php>Add New Teacher</a></td></tr>
 <tr><td><table width=750 cellspacing=0 cellpadding=5>
-<tr bgcolor=#ccccc><td align=center>S No</td><td align=center>Patient
-Name</td><td align=center>Doctor
-Name</td><td align=center>Time</td>
-<td align=center>Date</td><td
+<tr bgcolor=#ccccc><td align=center>Teacher ID No</td><td align=center>Teacher
+Name</td><td align=center>Subject ID</td><td align=center>Time Slot</td><td
 align=center>Options</td></tr>
 <?php
 
-if($_GET['rno']!=null)
-{
-$todel=$_GET['rno'];
-mysql_query("update appt SET ashow='N' where ano='$todel' ;");
-}
-$rs1=mysql_query("SELECT * from appt where ashow='Y'");
+
+$rs1=mysql_query("SELECT * from teacher where ashow='Y' ;");
 $sno=1;
 while( $row=mysql_fetch_array($rs1)) {
-
-$rs2=mysql_query("SELECT pname from patient where pno='$row[2]'");
-$rs3=mysql_query("SELECT dname from doct where dno='$row[1]'");
-$rs22=mysql_fetch_assoc($rs2);
-$rs33=mysql_fetch_assoc($rs3);
- echo "<tr><td>$sno</td><td>$rs22[pname]</td><td>$rs33[dname]</td><td>$row[3]</td><td>$row[5]</td><td><a
-href=alist.php?rno=".$row[0].">Delete</a></td></tr>";
+ echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td><a
+href=pmod.php?rno=".$row[0].">Modify</a> | <a
+href=pdel.php?rno=".$row[0].">Delete</a></td></tr>";
  $sno++;
 }
 if ($sno==1) echo "<tr><td align=center><font size=4 color=red>Records
-Not Found</font></td></tr>";?>
+Not Found</font></td></tr>";
+?>
 </table></td></tr>
 <tr><td align=center><hr></td></tr>
 <tr bgcolor=red><td><font size=4 color=white>Deleted
 Records</font></td></tr>
 <tr><td><table width=750 cellspacing=0 cellpadding=5>
-<tr bgcolor=#ccccc><td align=center>S No</td><td align=center>Patient
-Name</td><td align=center>Doctor
-Name</td><td align=center>Time</td>
-<td align=center>Date</td><td
+<tr bgcolor=#ccccc><td align=center>S No</td><td align=center>Course Name</td><td align=center>Address</td><td
 align=center>Options</td></tr>
 <?php
-$rs1=mysql_query("SELECT * from appt where ashow='N'");
+$rs2=mysql_query("SELECT * from patient where pshow='N' order by
+pname;");
 $sno=1;
-while( $row=mysql_fetch_array($rs1)) {
-
-$rs2=mysql_query("SELECT pname from patient where pno='$row[2]'");
-$rs3=mysql_query("SELECT dname from doct where dno='$row[1]'");
-$rs22=mysql_fetch_assoc($rs2);
-$rs33=mysql_fetch_assoc($rs3);
- echo "<tr><td>$sno</td><td>$rs22[pname]</td><td>$rs33[dname]</td><td>$row[3]</td><td>$row[5]</td><td><a
-href=aundel.php?rno=".$row[0].">Undelete</a></td></tr>";
+while( $row=mysql_fetch_array($rs2)) {
+ echo "<tr><td>$sno</td><td>$row[1]</td><td>$row[2]</td><td><a
+href=Pundel.php?rno=".$row[0].">Undelete</a></td></tr>";
  $sno++;
 }
 if ($sno==1) echo "<tr><td align=center><font size=4 color=red>Records
